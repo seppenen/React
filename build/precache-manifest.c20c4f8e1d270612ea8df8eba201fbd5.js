edca28c28bfeff0ea33fc883fe3d6b6742447b90
Aleksandr Seppenen <alex.seppenen@gmail.com>
2020-09-23 21:21:53 +0300

    build

---
+++ build/precache-manifest.c20c4f8e1d270612ea8df8eba201fbd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e696219b364d92402287889ae21bf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d06f6a735638cb2b652",
+    "url": "/static/js/2.0fe80fa7.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.0fe80fa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deeeece67d2ba59d26d5",
+    "url": "/static/js/main.f76b9e5d.chunk.js"
+  },
+  {
+    "revision": "a74b701c37427e685cc4",
+    "url": "/static/js/runtime-main.1f288b7d.js"
+  }
+]);
